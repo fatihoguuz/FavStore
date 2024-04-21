c46b74397604bcffca1cc8cebf4079e2f18342aa
Fatih Oguz <oguzfatih39@gmail.com>
2024-04-21 16:51:15 +0300

    Initial Commit

---
+++ FavStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FavStore
+//
+//  Created by Fatih Oğuz on 21.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
